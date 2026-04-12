86ac2bc9df391fd55a4ba9cec1eb68796b97a7a2
Justin Hoffman <jhffmn.myAlt1@gmail.com>
2026-04-12 09:38:09 -0500

    Add files via upload

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,3 @@
 from .wordle_env import WordleGame, load_word_list, get_feedback, filter_words
 from .state_encoder import encode_state, encode_words_onehot, STATE_DIM
-from .word_lists import build_curated_lists, get_sets
+from .word_lists import build_curated_lists, get_all_sets
